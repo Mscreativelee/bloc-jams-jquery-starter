475f780242227477eec564b6783638e86ddf7c6d
Mylene Suaco Hasselrot <mscreativesolutionsllc@gmail.com>
2019-03-07 10:56:16 -0500

    checkpoint 25sliders:revised

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -4,19 +4,6 @@
     $(this).attr('playState', player.playState);
   });
 
-  $('button#previous').on('click', function() {
-    if (player.playState !== 'playing') { return; }
-
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex - 1;
-    if (nextSongIndex >= album.songs.length) { return; }
-
-    const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
-  });
-
-
-
   $('button#next').on('click', function() {
     if (player.playState !== 'playing') { return; }
 
@@ -29,21 +16,21 @@
   });
 
   $('#time-control input').on('input', function (event) {
-    player.skipTo(event.target.value);
+     player.skipTo(event.target.value);
   });
 
   $('#volume-control input').on('input', function (event) {
-    player.setVolume(event.target.value);
+     player.setVolume(event.target.value);
   });
-
 
   setInterval( () => {
     if (player.playState !== 'playing') { return; }
     const currentTime = player.getTime();
     const duration = player.getDuration();
-    const percent = (currentTime / duration) *100;
+    const percent = (currentTime / duration) * 100;
     $('#time-control .current-time').text( currentTime );
     $('#time-control input').val(percent);
-  }, 1000);
+    $('#time-control .total-time').text( duration);
+   }, 1000);
 
-}  
+}
