8e3e4d153167682d7c0d9a19f0dd324beeb78f79
Mylene Suaco Hasselrot <mscreativesolutionsllc@gmail.com>
2019-03-10 20:26:10 -0400

    checkpoint sliders: revised3

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -3,6 +3,17 @@
     player.playPause();
     $(this).attr('playState', player.playState);
   });
+  $('button#previous').on('click', function() {
+      if (player.playState !== 'playing') { return; }
+
+       const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const nextSongIndex = currentSongIndex - 1;
+      if (nextSongIndex >= album.songs.length) { return; }
+
+       const nextSong = album.songs[nextSongIndex];
+      player.playPause(nextSong);
+    });
+
 
   $('button#next').on('click', function() {
     if (player.playState !== 'playing') { return; }
@@ -14,6 +25,7 @@
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
+
 
   $('#time-control input').on('input', function (event) {
      player.skipTo(event.target.value);
